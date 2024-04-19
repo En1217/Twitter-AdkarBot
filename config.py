e102b7f85132a014230927b1fdb737363cc8d455
En1217 <167570762+En1217@users.noreply.github.com>
2024-04-19 21:33:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ developer.twitter.com
 """    
 consumer_key = "xxxxxx"
 consumer_secret = "xxxxxx"
-access_token= "xxxxxx"
-access_secret = "xxxxxx"
+access_token= "820412749243121665-1SDtPwPZZ3LntWJiEAVIvOd2NJBm8P7"
+access_secret = "PMbtKJnUfNePZMTNCt5xtwd5dVJ7WqFFkEMtLXj2JQ1uc"
 
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
